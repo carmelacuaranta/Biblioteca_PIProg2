a77f0cb597183daa108b5bc110938257d8a4af14
Carmela Cuaranta <carmelalucuaranta@gmail.com>
2025-07-21 14:07:41 -0300

    correcciones en prestamos

--- Biblioteca/src/ArchivoPrestamo.cpp
+++ Biblioteca/src/ArchivoPrestamo.cpp
@@ -5,35 +5,30 @@
 using namespace std;
 
 int ArchivoPrestamo::agregarPrestamo(Prestamo pres){
-    //para avlidar que existan los id de socio y libro
-    ArchivoSocio soc;
     ArchivoLibro lib;
-    bool idSocioValido = soc.buscarSocioPorID(pres.getIdSocio());
-    bool idLibroValido = lib.buscarLibroPorID(pres.getIdLibro());
-
-    FILE *pPrestamo;
-    pPrestamo=fopen("Prestamos.dat", "ab");
-    if(pPrestamo==nullptr) {
+    ArchivoSocio soc;
+    int idSocioValido = soc.buscarSocioPorID(pres.getIdSocio());
+    int idLibroValido = lib.buscarLibroPorID(pres.getIdLibro());
+    FILE *pPrestamo = fopen("prestamos.dat", "ab");
+    if(pPrestamo == nullptr) {
         cout << "Error de archivo." << endl;
         return -1;
     }
 
-    if (idSocioValido == true && idLibroValido == true){
+    pres.setEstado(true);
+    if (idSocioValido >= 0 && idLibroValido >=0){
         fwrite(&pres, sizeof pres, 1, pPrestamo);
-        cout << "Prestamo registrdo correctamente." << endl;
-    } else {
-        cout << "No se puede realizar el prestamo." << endl;
+        cout << "Prestamo registrado." << endl;
     }
 
+
     fclose(pPrestamo);
-
-
     return 0;
 }
 
 bool ArchivoPrestamo::listarPrestamos() {
-    FILE* pPrestamo = fopen("Prestamos.dat", "rb");
-    if (pPrestamo == nullptr) {
+    FILE* p = fopen("prestamos.dat", "rb");
+    if (p == nullptr) {
         cout << "Error de archivo." << endl;
         return -1;
     }
@@ -41,12 +36,14 @@ bool ArchivoPrestamo::listarPrestamos() {
     Prestamo pres;
     cout << "LISTADO DE PRESTAMOS:" << endl << "-------------------" << endl;
 
-    while (fread(&pres, sizeof(Prestamo), 1, pPrestamo) == 1) {
-        pres.mostrarPrestamo();
-        cout << "-------------------" << endl;
+    while (fread(&pres, sizeof(Prestamo), 1, p) == 1) {
+        if(pres.getEstado()==true){
+            pres.mostrarPrestamo();
+            cout << "-------------------" << endl;
+        }
     }
 
-    fclose(pPrestamo);
+    fclose(p);
     return 0;
 }
 
